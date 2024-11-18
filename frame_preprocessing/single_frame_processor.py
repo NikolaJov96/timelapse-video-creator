d2dc5b9bd249dbbe6dd33327f19d1bd26456acca
Nikola Jovanović <jovanovicn.96@gmail.com>
2024-11-18 01:21:09 +0100

    Refactor `process_frame` method

--- frame_preprocessing/single_frame_processor.py
+++ frame_preprocessing/single_frame_processor.py
@@ -29,43 +29,33 @@ class SingleFrameProcessor:
             image_data: ImageData) -> None:
         """
         """
-        frame_timestamp_s = image_data.timestamp_s
-        if self.__options.ignore_daylight_savings_switch:
-            is_frame_in_daylight_savings = DatetimeUtils.is_in_daylight_savings(
-                frame_timestamp_s, self.__options.timezone)
-            if self.__is_first_frame_in_daylight_savings and not is_frame_in_daylight_savings:
-                frame_timestamp_s -= 60 * 60
-            elif not self.__is_first_frame_in_daylight_savings and is_frame_in_daylight_savings:
-                frame_timestamp_s += 60 * 60
+        frame_timestamp_s = self.__get_daylight_savings_adjusted_timestamp(image_data.timestamp_s)
+        sunrise, sunset = self.__get_sunrise_sunset(
+            frame_timestamp_s, self.__options.latitude, self.__options.longitude)
 
-        sun = SunTimes(
-            longitude=image_data.longitude,
-            latitude=image_data.latitude,
-            altitude=0)
-        sun_rise = sun.risewhere(date.fromtimestamp(frame_timestamp_s), self.__options.timezone)
-        sun_set = sun.setwhere(date.fromtimestamp(frame_timestamp_s), self.__options.timezone)
+        # The times of the earliest and the latest frame for the current day
+        earliest_frame_timestamp_s = sunrise.timestamp() - self.__options.night_margin_seconds
+        latest_frame_timestamp_s = sunset.timestamp() + self.__options.night_margin_seconds
 
-        earliest_frame_timestamp_s = sun_rise.timestamp() - self.__options.night_margin_seconds
-        latest_frame_timestamp_s = sun_set.timestamp() + self.__options.night_margin_seconds
-
+        # How close the current frame is to the earliest and the latest frame in seconds
         seconds_since_earliest_frame = frame_timestamp_s - earliest_frame_timestamp_s
         seconds_until_latest_frame = latest_frame_timestamp_s - frame_timestamp_s
 
+        # If the current frame is outside the range of the earliest and the latest frame, skip it
         if seconds_since_earliest_frame < 0 or seconds_until_latest_frame < 0:
             return
 
+        # Check if the current frame is in the fade range of the earliest or the latest frame
         close_to_earliest_frame = 0 <= seconds_since_earliest_frame <= self.__options.fade_seconds
         close_to_latest_frame = 0 <= seconds_until_latest_frame <= self.__options.fade_seconds
 
-        image_id_str = f'{image_id:010d}'
-        image_time_str = datetime.fromtimestamp(frame_timestamp_s).strftime('%Y_%m_%d_%H_%M_%S')
-        night_flag = self.__get_night_flag(frame_timestamp_s, sun_rise, sun_set)
-        daylight_savings_flag = '_d' if frame_timestamp_s != image_data.timestamp_s else ''
-        new_image_name = f'{image_id_str}_{image_time_str}{night_flag}{daylight_savings_flag}.jpg'
-        subfolder_path = self.__options.output_dir / f'{image_id // self.MAX_IMAGES_PER_FOLDER:03d}'
-        new_image_path = subfolder_path / new_image_name
+        # Get output image path and create the parent directories if needed
+        new_image_path = self.__generate_output_image_path(
+            image_id, frame_timestamp_s, sunrise, sunset, image_data.timestamp_s)
+        new_image_path.parent.mkdir(parents=True, exist_ok=True)
 
         if close_to_earliest_frame or close_to_latest_frame:
+            # The frame is close to the earliest or the latest frame, apply the fade effect
             if close_to_earliest_frame:
                 progress = seconds_since_earliest_frame / self.__options.fade_seconds
             else:
@@ -74,17 +64,60 @@ class SingleFrameProcessor:
 
             image = cv2.imread(str(image_path))
             image = (image * progress).astype('uint8')
-            subfolder_path.mkdir(parents=True, exist_ok=True)
             cv2.imwrite(str(new_image_path), image)
         else:
-            subfolder_path.mkdir(parents=True, exist_ok=True)
+            # The frame is not close to the earliest or the latest frame, just copy it
             shutil.copy(image_path, new_image_path)
 
-    def __get_night_flag(self, timestamp_s: int, sun_rise: datetime, sun_set: datetime) -> str:
+    def __get_daylight_savings_adjusted_timestamp(self, timestamp_s: int) -> int:
         """
         """
-        seconds_since_sunrise = timestamp_s - sun_rise.timestamp()
-        seconds_until_sunset = sun_set.timestamp() - timestamp_s
+        adjusted_timestamp_s = timestamp_s
+        if self.__options.ignore_daylight_savings_switch:
+            is_frame_in_daylight_savings = DatetimeUtils.is_in_daylight_savings(
+                adjusted_timestamp_s, self.__options.timezone)
+            if self.__is_first_frame_in_daylight_savings and not is_frame_in_daylight_savings:
+                adjusted_timestamp_s -= 60 * 60
+            elif not self.__is_first_frame_in_daylight_savings and is_frame_in_daylight_savings:
+                adjusted_timestamp_s += 60 * 60
+
+        return adjusted_timestamp_s
+
+    def __get_sunrise_sunset(self, timestamp_s: int, latitude: float, longitude: float) -> tuple[datetime, datetime]:
+        """
+        """
+        sun = SunTimes(longitude=longitude, latitude=latitude, altitude=0)
+        sunrise = sun.risewhere(date.fromtimestamp(timestamp_s), self.__options.timezone)
+        sunset = sun.setwhere(date.fromtimestamp(timestamp_s), self.__options.timezone)
+
+        return sunrise, sunset
+
+    def __generate_output_image_path(
+            self,
+            image_id: int,
+            frame_timestamp_s: int,
+            sunrise: datetime,
+            sunset: datetime,
+            non_adjusted_timestamp_s: int) -> pathlib.Path:
+        """
+        """
+        image_id_str = f'{image_id:010d}'
+        image_time_str = datetime.fromtimestamp(frame_timestamp_s).strftime('%Y_%m_%d_%H_%M_%S')
+        night_flag = self.__get_night_flag(frame_timestamp_s, sunrise, sunset)
+        daylight_savings_flag = '_d' if frame_timestamp_s != non_adjusted_timestamp_s else ''
+
+        new_image_name = f'{image_id_str}_{image_time_str}{night_flag}{daylight_savings_flag}.jpg'
+
+        # Create subfolders with up to 500 images each to avoid having too many files in one directory
+        subfolder_path = self.__options.output_dir / f'{image_id // self.MAX_IMAGES_PER_FOLDER:03d}'
+
+        return subfolder_path / new_image_name
+
+    def __get_night_flag(self, timestamp_s: int, sunrise: datetime, sunset: datetime) -> str:
+        """
+        """
+        seconds_since_sunrise = timestamp_s - sunrise.timestamp()
+        seconds_until_sunset = sunset.timestamp() - timestamp_s
 
         is_before_sunrise = seconds_since_sunrise < 0
         is_after_sunset = seconds_until_sunset < 0
